d6b0c4a1757aac1f908cf0d08674a08f9cf0ed4d
Csongor Halmai <halmai@users.noreply.github.com>
2019-09-23 17:40:31 +1000

    let the script run on every PR-related page

--- userscript.js
+++ userscript.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Csongor Halmai
-// @match        https://github.com/*/pull/4/files
+// @match        https://github.com/*/pull/*
 // @grant        none
 // ==/UserScript==
 
