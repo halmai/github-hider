02f891a88a3c0e9532253d45a905725f5e871b3e
Csongor Halmai <halmai@users.noreply.github.com>
2020-06-10 17:50:48 +1000

    filter both commits and pull requests

--- userscript.js
+++ userscript.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       Csongor Halmai
 // @match        https://github.com/*/pull/*
+// @match        https://github.com/*/commit/*
 // @grant        none
 // ==/UserScript==
 
